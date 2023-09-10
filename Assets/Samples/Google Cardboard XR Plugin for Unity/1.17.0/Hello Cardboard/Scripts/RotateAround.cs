5afb794bb56ae043bcadb630c50f39c9a5f004b4
kikiman001 <enriquepriego505@gmail.com>
2023-09-09 21:28:12 -0600

    practica2

---
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.17.0/Hello Cardboard/Scripts/RotateAround.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public Transform centroDelCarrusel;
+    public float velocidadRotacion = 30f;
+	public float retrasoInicial = 0f; // El primer hijo no tiene retraso.
+
+    private float[] tiemposDeRetraso; // Almacena los tiempos de retraso para cada hijo.
+
+    void Start()
+    {
+        // Inicializa los tiempos de retraso para cada hijo.
+        tiemposDeRetraso = new float[transform.childCount];
+        for (int i = 0; i < tiemposDeRetraso.Length; i++)
+        {
+            tiemposDeRetraso[i] = retrasoInicial + i * 30f; // Aumenta el retraso para cada hijo.
+        }
+    }
+
+    void Update()
+    {
+        // Rotar todas las imágenes alrededor del centro con su retraso respectivo.
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform imagen = transform.GetChild(i);
+            float tiempoDeRetraso = tiemposDeRetraso[i];
+
+            if (Time.time >= tiempoDeRetraso)
+            {
+                imagen.RotateAround(centroDelCarrusel.position, Vector3.up, velocidadRotacion * Time.deltaTime);
+            }
+        }
+    }
+}
