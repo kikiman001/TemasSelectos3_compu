f19d89dd80c8401d5b3683041d26d2c2fa7e5425
kikiman001 <enriquepriego505@gmail.com>
2023-09-12 20:07:33 -0600

    Update RotateAround.cs

--- Assets/Samples/Google Cardboard XR Plugin for Unity/1.17.0/Hello Cardboard/Scripts/RotateAround.cs
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.17.0/Hello Cardboard/Scripts/RotateAround.cs
@@ -1,37 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateAround : MonoBehaviour
 {
     public Transform centroDelCarrusel;
     public float velocidadRotacion = 30f;
-    public float retrasoInicial = 0f; // El primer hijo no tiene retraso.
+    public float tiempoVisible = 5f; // Tiempo en segundos para que cada grupo sea visible.
+    public float retardoInicial = 2f; // Retardo inicial antes de que comience el movimiento.
 
-    private float[] tiemposDeRetraso; // Almacena los tiempos de retraso para cada hijo.
+    private Transform[] hijos;
+    private int grupoActual = 0;
+    private float tiempoUltimoCambio;
+    private int objetosPorGrupo = 3;
 
     void Start()
     {
-        // Inicializa los tiempos de retraso para cada hijo.
-        tiemposDeRetraso = new float[transform.childCount];
-        for (int i = 0; i < tiemposDeRetraso.Length; i++)
+        // Obtén los hijos del objeto actual.
+        hijos = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
         {
-            tiemposDeRetraso[i] = retrasoInicial + i * 1.8f; // Aumenta el retraso para cada hijo.
+            hijos[i] = transform.GetChild(i);
+            // Haz que todos los objetos estén inicialmente invisibles.
+            hijos[i].gameObject.SetActive(false);
         }
+
+        tiempoUltimoCambio = Time.time + retardoInicial;
     }
 
     void Update()
     {
-        // Rotar todas las imágenes alrededor del centro con su retraso respectivo.
-        for (int i = 0; i < transform.childCount; i++)
+        // Verifica si es hora de cambiar el grupo visible.
+        if (Time.time - tiempoUltimoCambio >= tiempoVisible)
         {
-            Transform imagen = transform.GetChild(i);
-            float tiempoDeRetraso = tiemposDeRetraso[i];
-
-            if (Time.time >= tiempoDeRetraso)
-            {
-                imagen.RotateAround(centroDelCarrusel.position, Vector3.up, velocidadRotacion * Time.deltaTime);
-            }
+            CambiarGrupoVisible();
         }
+
+        // Rotar todas las imágenes alrededor del centro.
+        foreach (Transform hijo in hijos)
+        {
+            hijo.RotateAround(centroDelCarrusel.position, Vector3.down, velocidadRotacion * Time.deltaTime);
+        }
+    }
+
+    void CambiarGrupoVisible()
+    {
+        for (int i = 0; i < hijos.Length; i++)
+        {
+            int grupo = i / objetosPorGrupo;
+            bool deberiaSerVisible = grupo == grupoActual;
+
+            // Activa o desactiva el objeto según el grupo.
+            hijos[i].gameObject.SetActive(deberiaSerVisible);
+        }
+
+        // Actualiza el tiempo del último cambio y avanza al siguiente grupo.
+        tiempoUltimoCambio = Time.time;
+        grupoActual = (grupoActual + 1) % (hijos.Length / objetosPorGrupo);
     }
 }
