96c14cc77abf695c901ad51867296c1c88f78a3c
kikiman001 <enriquepriego505@gmail.com>
2023-10-30 17:52:01 -0600

    P5
    Seguimiento de enemigo a usuario, se cambio en meshAgent y se agrego camara para seguimiento en Arisa
    se modifico el navMesh

--- Assets/Samples/Google Cardboard XR Plugin for Unity/1.17.0/Hello Cardboard/Scripts/Enemy.cs
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.17.0/Hello Cardboard/Scripts/Enemy.cs
@@ -6,24 +6,41 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
-    //public bool userDetect;
+    public bool userDetect;
     public Transform user;
     private NavMeshAgent enemy;
     // lo que se recomienda es dejarlo en privado
-    //public Animator EnemyAnimator;
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    userDetect = true;
-    //}
+    public Animator EnemyAnimator;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            userDetect = true;
+            EnemyAnimator.SetBool("punch", true); // Activa la animación de golpear
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            userDetect = true;
+            EnemyAnimator.SetBool("punch", false); // Desactiva la animación de golpear al salir de la colisión
+        }
+    }
+
     void Start()
     {
         enemy = GetComponent<NavMeshAgent>();
-        //EnemyAnimator = GetComponent<Animator>();
+        EnemyAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemy.destination = user.position;
+
+        if (userDetect)
+        {
+            enemy.destination = user.position;
+        }
     }
 }
