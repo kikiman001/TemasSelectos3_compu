2d007a94297365554a1bf9024f03b12252a32038
kikiman001 <enriquepriego505@gmail.com>
2023-10-29 20:19:40 -0600

    p5
    reload nav

--- Assets/Samples/Google Cardboard XR Plugin for Unity/1.17.0/Hello Cardboard/Scripts/Enemy.cs
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.17.0/Hello Cardboard/Scripts/Enemy.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine;
+
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Transform objetivo;
-    NavMeshAgent agente;
+    //public bool userDetect;
+    public Transform user;
+    private NavMeshAgent enemy;
+    // lo que se recomienda es dejarlo en privado
+    //public Animator EnemyAnimator;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    userDetect = true;
+    //}
     void Start()
     {
-        agente = GetComponent<NavMeshAgent>();
-        
+        enemy = GetComponent<NavMeshAgent>();
+        //EnemyAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agente.destination = objetivo.position;
+        enemy.destination = user.position;
     }
 }
